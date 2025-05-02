c57c5459bd8f44148c4bb02fcd44c5ddac2dd450
F3rd3f <f3rd3f@gmail.com>
2025-05-03 01:22:27 +0200

    Updated Node JS version

--- fibonacci/NodeJS/main.js
+++ fibonacci/NodeJS/main.js
@@ -1,12 +1,18 @@
 function fibo(num) {
   if (num < 2) {
-    return num;
+    return 1;
   }
 
   return fibo(num-1) + fibo(num-2);
 }
 
-const num = 50;
+const num = 45;
+
+const startTime = performance.now();
+
 var result = fibo(num);
 
+const endTime = performance.now();
+
 console.log("Fibo of %d is %d", num, result);
+console.log(`Took ${endTime - startTime} ms`);
