b14e4b21152b31420a9ffcf65e245a22b0fac437
F3rd3f <f3rd3f@gmail.com>
2025-05-04 17:38:40 +0200

    Skeleton for Mandelbrot's Typescript version

---
+++ mandelbrot/TypeScript/dist/index.js
@@ -0,0 +1,8 @@
+"use strict";
+function main() {
+}
+const startTime = performance.now();
+main();
+const endTime = performance.now();
+const duration = endTime - startTime;
+console.log(`Time elapsed ${duration} ms`);
---
+++ mandelbrot/TypeScript/src/index.ts
@@ -0,0 +1,10 @@
+function main() {
+
+}
+
+const startTime = performance.now()
+main();
+const endTime = performance.now()
+const duration = endTime - startTime;
+
+console.log(`Time elapsed ${duration} ms`);
