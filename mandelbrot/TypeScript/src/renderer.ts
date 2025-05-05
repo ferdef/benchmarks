9415dd3a1199930a9b092b733be5f301c6582bba
Ferdef <fernando@promaton.com>
2025-05-05 17:23:42 +0200

    Improving TS version speed

--- mandelbrot/TypeScript/src/renderer.ts
+++ mandelbrot/TypeScript/src/renderer.ts
@@ -12,9 +12,16 @@ export function render(
     exit(1);
   }
 
+  const width = lowerRight.re - upperLeft.re;
+  const height = upperLeft.im - lowerRight.im;
+
   for(let row = 0; row < bounds[1]; row++) {
     for(let column = 0; column < bounds[0]; column++) {
-      let point = pixelToPoint(bounds, [column, row], upperLeft, lowerRight);
+
+      const re = upperLeft.re + column * width / bounds[0];
+      const im = upperLeft.im - row * height / bounds[1];
+      const point = new Complex(re, im);
+
       let et = escapeTime(point, 255);
       pixels[row * bounds[0] + column] = (et === null) ? 0 : 255 +- et;
     }
@@ -22,26 +29,44 @@ export function render(
 }
 
 function escapeTime(c: Complex, limit: number): number | null {
-  let z = new Complex(0.0, 0.0);
-  for(let it = 0; it < limit; it++) {
-    if (z.normSqr() > 4.0) {
+  let zr = 0.0, zi = 0.0;
+  for (let it = 0; it < limit; it++) {
+    const zr2 = zr * zr;
+    const zi2 = zi * zi;
+
+    if (zr2 + zi2 > 4.0) {
       return it;
     }
 
-    z = z.multiply(z).add(c);
+    const temp = zr2 - zi2 + c.re;
+    zi = 2.0 * zr * zi + c.im;
+    zr = temp;
   }
 
   return null;
 }
 
-function pixelToPoint(
-  bounds: [number, number],
-  pixel: [number, number],
-  upperLeft: Complex,
-  lowerRight: Complex): Complex {
-  let [width, height] = [lowerRight.re - upperLeft.re, upperLeft.im - lowerRight.im];
-  return new Complex(
-    upperLeft.re + pixel[0] * width / bounds[0],
-    upperLeft.im - pixel[1] * height / bounds[1]
-  );
-}
+// function escapeTime(c: Complex, limit: number): number | null {
+//   let z = new Complex(0.0, 0.0);
+//   for(let it = 0; it < limit; it++) {
+//     if (z.normSqr() > 4.0) {
+//       return it;
+//     }
+
+//     z = z.multiply(z).add(c);
+//   }
+
+//   return null;
+// }
+
+// function pixelToPoint(
+//   bounds: [number, number],
+//   pixel: [number, number],
+//   upperLeft: Complex,
+//   lowerRight: Complex): Complex {
+//   let [width, height] = [lowerRight.re - upperLeft.re, upperLeft.im - lowerRight.im];
+//   return new Complex(
+//     upperLeft.re + pixel[0] * width / bounds[0],
+//     upperLeft.im - pixel[1] * height / bounds[1]
+//   );
+// }
