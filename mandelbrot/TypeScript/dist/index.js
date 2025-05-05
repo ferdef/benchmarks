977469155019e1b9ae649cff804789339d795900
F3rd3f <f3rd3f@gmail.com>
2025-05-05 07:38:29 +0200

    removed javascript file

--- mandelbrot/TypeScript/dist/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-function main() {
-}
-const startTime = performance.now();
-main();
-const endTime = performance.now();
-const duration = endTime - startTime;
-console.log(`Time elapsed ${duration} ms`);
